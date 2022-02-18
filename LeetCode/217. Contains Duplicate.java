4ac533fc2d2a7918645d28a0c52dfc8ad00a0b74
19ray93 <subhradeepray2017@gmail.com>
2022-02-19 00:44:21 +0530

    217. Contains Duplicate.java

---
+++ LeetCode/217. Contains Duplicate.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+    int n = nums.length;
+	Map<Integer, Integer> mp= new HashMap<>();
+	for(int i = 0; i < n; i++) {
+	    if(mp.containsKey(nums[i])){
+		mp.put(nums[i], mp.get(nums[i]) + 1);
+    	    } else {
+		mp.put(nums[i], 1);	
+	    }
+	}
+	for(Map.Entry<Integer, Integer> entry : mp.entrySet())
+	if(entry.getValue() > 1)
+	return true;
+	return false;
+    }
+}
+
+//Time Complexity : O(n) Space Complexity: O(n)
+
